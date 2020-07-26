dd1a2a4103d8aa883e7d1b4b2ded4a58430a6dbf
shenm <shenmq@belink.com>
2020-07-26 21:13:59 +0800

    first commit

---
+++ src/main/java/com/ynet/ifp/example/controller/ExampleController.java
@@ -0,0 +1,15 @@
+package com.ynet.ifp.example.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/example")
+    public String example() {
+        return "example";
+    }
+
+}
